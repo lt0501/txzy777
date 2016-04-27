a468722055c705563afa32e91ac620719742b693
LiTao <1287862489@qq.com>
2016-04-27 23:08:25 +0800

    cms

---
+++ Home/Lib/Widget/NavWidget.class.php
@@ -0,0 +1,31 @@
+<?php
+class NavWidget extends Widget{
+	public function render($data){
+		$_template=$data['lang'].'_nav';
+		if(S('navdata')){
+			$data=S('navdata');
+		}else{
+			$n=M('List');
+			$nav=$n->field('id,name,ename,url,pid,type,link,elink')->where('pid=0 and nav=1')->order('sort asc')->select();
+			$data['nav']=$nav;//有效底部nav
+
+			$nav2=$n->field('id,name,ename,url,pid,type,link,elink')->where('pid=1 and nav=1')->order('sort asc')->select();
+			$data['nav2']=$nav2;//有效顶部nav
+			
+			$nav3=$n->field('id,name,ename,url,pid,type,link,elink')->where('pid=56')->order('sort asc')->select();
+			$data['nav3']=$nav3;//有效中间nav
+
+			if($nav2){
+				$snav=$n->field('id,name,ename,url,pid,type,link,elink')->where('pid != 0 and nav=1')->order('sort asc')->select();
+				$data['snav']=$snav;
+				S('navdata',$data,3600 * 24);
+			}
+		}
+
+		$content=$this->renderFile($_template,$data);
+		return $content;
+	}
+
+
+}
+?>
---
+++ index.php
@@ -0,0 +1,6 @@
+<?php
+define('APP_NAME','Home');
+define('APP_PATH','./Home/');
+define('APP_DEBUG',false);
+require './Framework/ThinkPHP.php';
+?>
