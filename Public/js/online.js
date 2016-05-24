b4f918d594069e47b1286cbee84ce47bf1935af3
LiTao <1287862489@qq.com>
2016-05-24 23:45:05 +0800

    cms

--- Public/js/online.js
+++ Public/js/online.js
@@ -1,8 +1,8 @@
 
     $(function(){
 
-        // cms客服浮动面板
-        if($("#cmsFloatPanel")){
+        // 客服浮动面板
+      if($("#cmsFloatPanel")){
 	  $("#cmsFloatPanel > .ctrolPanel > a.arrow").eq(0).click(function(){$("html,body").animate({scrollTop :0}, 800);return false;});
 	  $("#cmsFloatPanel > .ctrolPanel > a.arrow").eq(1).click(function(){$("html,body").animate({scrollTop : $(document).height()}, 800);return false;});
 	  var objServicePanel = $("#cmsFloatPanel > .servicePanel");
@@ -68,7 +68,7 @@
 			  return false;
 		  }
 	  });
-
+	  
         }
         
     });
